afa8f3f75b7cb2b3dad96c520567e3d8234facf5
Kai Wei <ga642381@gmail.com>
2022-10-14 16:30:58 +0800

    bug fixed

--- Allennlp/translation_api.py
+++ Allennlp/translation_api.py
@@ -11,13 +11,12 @@ class TranslationAPI():
         self.model = model
         self.this_path = pathlib.Path(__file__).parent.absolute()
         self.model_dir = os.path.join(self.this_path, 'model')
-        self.start_allen_server()
-        
+
     def start_allen_server(self):
         if self.model == 'transformer':
             model_name = 'bert_transformer'
             model_path = os.path.join(self.model_dir, f'{model_name}.tar.gz')
-        
+
         port = 8000
         port_using = self.check_port_usage(port)
         if port_using:
@@ -28,7 +27,7 @@ class TranslationAPI():
             print(cmd)
             self.proc = subprocess.Popen(cmd, shell=True)
             #self.proc = subprocess.Popen(cmd, stdout=PIPE, stderr=PIPE, shell=True)
-    
+
     def check_port_usage(self, port):
         cmd = f"fuser {port}/tcp"
         p = subprocess.run(cmd, capture_output=True, shell=True)
@@ -39,22 +38,17 @@ class TranslationAPI():
 
     def translate(self, txt):
         url = "http://127.0.0.1:8000/predict"
-        req_data = {"source":txt}
-        req_headers = {'Content-Type':'application/json'}
-        
+        req_data = {"source": txt}
+        req_headers = {'Content-Type': 'application/json'}
+
         rsp = requests.post(url, headers=req_headers, json=req_data)
-        translated_tokens =  eval(rsp.content.decode())['predicted_tokens']
+        translated_tokens = eval(rsp.content.decode())['predicted_tokens']
         return ' '.join(translated_tokens)
 
     def shut_down_server(self):
         self.proc.kill()
-        
+
+
 if __name__ == '__main__':
     API = TranslationAPI('transformer')
     API.start_allen_server()
-    
-    
-    
-    # print(proc.stdout.read().split('\n')[-2])
-    # OSError: [Errno 98] Address already in use: ('127.0.0.1', 8000)
-    # if running, no return
